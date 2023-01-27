d66ba5fe4a12fb89e8f8f55be775c307d13328e4
LearnedDr <fredrickj.chang@gmail.com>
2023-01-27 00:15:12 -0500

    intial push almost done

--- index.js
+++ index.js
@@ -1,13 +1,85 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs/promises');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: 'title',
+        type: 'input',
+        message: 'Welcome to readme generator!  What is the name of your application?'
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Great name!  Give me a detailed description on how your application works.'
+    },
+    // add a table of contents
+    {
+        name: 'installation',
+        type: 'input',
+        message: 'Sounds interesting.  Explain how to install your application.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE2.0', 'GPL3.0', 'BSD3', 'None'],
+      },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'Usage: What is best way to use your application?'
+    },
+    {
+        name: 'contribution',
+        type: 'input',
+        message: 'After seeing how awesome your application is, how can other contribute?'
+    },
+    {
+        name: 'test',
+        type: 'input',
+        message: ''
+    },
+    // add questions section
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// const fileName = 'README.md'
+const data = ""
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data)
+        .then(()=> console.log("What a great README"))
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(response => {
+            console.log("response", response)
+    writeToFile('README.md', generateMarkdown(response))
+        });
+}
 
 // Function call to initialize app
 init();
+
+
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,64 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `\n ![github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `\n *[License](#license)
+    
+    `
+  };
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `\n ## License
+    This projecet is under the ${license} license
+    `
+  };
+  return "";
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
+    ## Description
+    ${data.description}
+
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Contribution](#contribution)
+    * [Test](#test)
+
+    ## renderthelinkhere
+    
+    ## Installation
+    ${data.installation}
+    
+    ## Usage
+    ${data.usage}
+
+    ## Contribution
+    ${data.contribution}
+
+    ##Test
+    ${data.test}
+
+    ## rendersectionhere
 `;
 }
 
